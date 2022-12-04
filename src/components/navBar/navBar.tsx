eb572f722da4e51a306f83b0f80db486f4716024
SantinoFajardo <santinofajardo02@gmail.com>
2022-12-04 11:48:10 -0300

    NavBar

--- src/components/navBar/navBar.tsx
+++ src/components/navBar/navBar.tsx
@@ -31,15 +31,17 @@ const NavBar: NextPage = () => {
   const [menu, setMenu] = useState(false);
   const openMenu = (): void => {
     setMenu(!menu);
+    console.log("Status:", menu);
   };
   return (
     <nav className="bg-white px-2 sm:px-4 py-2.5 fixed w-full z-20 top-0 left-0 border-b-blue-500 border-b-[.5px]">
       <div className="container flex flex-wrap items-center justify-between mx-auto">
         <a href="https://flowbite.com/" className="flex items-center">
           <Image
+            priority
             src={FametalLogo}
             className="h-[100px] w-[100px] bg-red-transparent absolute"
-            alt="SphereOne Logo"
+            alt="Fametal Logo"
           />
         </a>
         <div className="flex md:order-2">
@@ -66,7 +68,7 @@ const NavBar: NextPage = () => {
           </button>
         </div>
         <div
-          className={`md:flex md:items-center md:pb-0 pb-12 absolute md:static bg-transparent z-[-1] left-0 w-full h-full md:w-auto transition-all duration-500 ease-in ${
+          className={`md:flex md:items-center absolute md:pb-0 pb-12 md:static bg-transparent z-[-1] left-0 w-full h-full md:w-auto transition-all duration-500 ease-in ${
             menu ? "top-16" : "top-[-490px]"
           }`}
           id="navbar-sticky"
