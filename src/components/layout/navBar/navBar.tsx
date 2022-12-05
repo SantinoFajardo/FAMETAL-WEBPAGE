9c34fc2e98f204636acdf667ecb49a65450b66b1
SantinoFajardo <santinofajardo02@gmail.com>
2022-12-04 22:10:46 -0300

    NavBar blur

--- src/components/layout/navBar/navBar.tsx
+++ src/components/layout/navBar/navBar.tsx
@@ -26,7 +26,7 @@ const NavBar: NextPage = () => {
   };
   cookie.set("Lenguaje", lenguaje, { path: "/" });
   return (
-    <nav className="bg-[#5261EA] px-2 sm:px-4 py-2.5 fixed w-full z-20 top-0 left-0 border-b-blue-500 border-b-[.5px] backdrop-blur-xl">
+    <nav className="bg-[#5261EA]/80 px-2 sm:px-4 py-2.5 fixed w-full z-20 top-0 left-0 border-b-blue-500 border-b-[.5px] backrop-blur-xl">
       <div className="container flex flex-wrap items-center justify-between mx-auto">
         <a href="https://flowbite.com/" className="flex items-center">
           <Image
